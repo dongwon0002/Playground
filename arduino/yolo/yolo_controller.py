e839b7b0d0073db49125c7b17c8a61861bbe9232
dongwon0002 <dongwon4428@gmail.com>
2025-01-21 21:54:50 +0900

    Update yolo_controller.py

--- arduino/yolo/yolo_controller.py
+++ arduino/yolo/yolo_controller.py
@@ -19,9 +19,9 @@ def led(cls):
         led_1.write(1)
         last_detected = time.time()  # 'dog'가 탐지된 시간을 기록
     elif last_detected is not None:
-        # 'dog'가 더 이상 탐지되지 않으면, 마지막으로 탐지된 시간이 10초 이상 경과하면 LED를 끈다.
+        # 'dog'가 더 이상 탐지되지 않으면, 마지막으로 탐지된 시간이 1초 이상 경과하면 LED를 끈다.
         if time.time() - last_detected > 1:
-            led_1.write(0)  # 10초 경과 후 LED 끄기
+            led_1.write(0)  # 1초 경과 후 LED 끄기
             last_detected = None  # 타이머 초기화
         else:
             # 'dog'가 탐지되지 않은 경우 계속 켜두기
